9ecc1f56cd07809dcac23850f0b6317391ceb974
UmerIshaq <140231923+UmerIshaq@users.noreply.github.com>
2024-06-30 17:11:49 +0300

    fixed

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,7 +1,8 @@
 "use client";
+
+import { useState } from "react";
 import { Button } from "../../components/ui/button";
 import { data } from "./questions";
-import { useState } from "react";
 import {
   Card,
   CardContent,
@@ -12,125 +13,101 @@ import { cn } from "@/lib/utils";
 
 const QuizPage = () => {
   const [index, setIndex] = useState(0);
-  const [selected, setSelected] = useState();
+  const [selected, setSelected] = useState(null);
   const [score, setScore] = useState(0);
   const quiz = data[index];
-  const HandleIndex = () => {
-    setSelected();
+
+  const handleNext = () => {
+    setSelected(null);
     setIndex(index + 1);
   };
+
+  const handleAnswer = (option, selectedIndex) => {
+    if (option === quiz.answer) {
+      setScore(score + 1);
+    }
+    setSelected(selectedIndex);
+  };
+
   return (
     <div className="w-full h-screen bg-slate-300">
       <div className="">
-        {index < 15 ? (
+        {index < data.length ? (
           <div>
             <div className="flex flex-col gap-2 justify-center items-center py-3">
               <h1 className="text-4xl font-bold font-serif text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-fuchsia-600">
                 Quiz Page
               </h1>
-              <h3 className="text-2xl font-semibold ">
-                Questions{data.length}
+              <h3 className="text-2xl font-semibold">
+                Question {index + 1}/{data.length}
               </h3>
             </div>
-
             <div className="w-full">
-              <div>
-                <Card className={"w-full py-4 px-4 flex flex-col gap-7"}>
-                  <div>
-                    <CardHeader
-                      className={
-                        "text-lg sm:text-lg md:text-3xl font-bold px-1 h-20"
-                      }
-                    >
-                      <h1>
-                        {index + 1}.{quiz.question}
-                      </h1>
-                    </CardHeader>
-                  </div>
-                  <div className="flex flex-col gap-3">
-                    <CardContent
-                      onClick={() => {
-                        quiz.optionA == quiz.answer
-                          ? setScore(score + 1)
-                          : null;
-                        setSelected(0);
-                      }}
-                      className={cn(
-                        `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer `,
-                        selected == 0
-                          ? "bg-blue-600 hover:bg-blue-600"
-                          : "bg-white"
-                      )}
-                    >
-                      {quiz.optionA}
-                    </CardContent>
-                    <CardContent
-                      onClick={() => {
-                        quiz.optionB == quiz.answer
-                          ? setScore(score + 1)
-                          : null;
-                        setSelected(1);
-                      }}
-                      className={cn(
-                        `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer `,
-                        selected == 1
-                          ? "bg-blue-600 hover:bg-blue-600"
-                          : "bg-white"
-                      )}
-                    >
-                      {quiz.optionB}
-                    </CardContent>
-                    <CardContent
-                      onClick={() => {
-                        quiz.optionC == quiz.answer
-                          ? setScore(score + 1)
-                          : null;
-                        setSelected(2);
-                      }}
-                      className={cn(
-                        `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer`,
-                        selected == 2
-                          ? "bg-blue-600 hover:bg-blue-600"
-                          : "bg-white"
-                      )}
-                    >
-                      {quiz.optionC}
-                    </CardContent>
-                    <CardContent
-                      onClick={() => {
-                        quiz.optionD == quiz.answer
-                          ? setScore(score + 1)
-                          : null;
-                        setSelected(3);
-                      }}
-                      className={cn(
-                        `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer`,
-                        selected == 3
-                          ? "bg-blue-600 hover:bg-blue-600"
-                          : "bg-white"
-                      )}
-                    >
-                      {quiz.optionD}
-                    </CardContent>
-                  </div>
-                  <div className=" w-full float-end">
-                    <Button
-                      className={
-                        "w-full md:w-[100px] py-6 hover:bg-gradient-to-r hover:from-blue-600 hover:to-fuchsia-600"
-                      }
-                      onClick={() => {
-                        HandleIndex();
-                      }}
-                    >
-                      Next
-                    </Button>
-                  </div>
-                </Card>
-              </div>
+              <Card className="w-full py-4 px-4 flex flex-col gap-7">
+                <CardHeader className="text-lg sm:text-lg md:text-3xl font-bold px-1 h-20">
+                  <h1>
+                    {index + 1}. {quiz.question}
+                  </h1>
+                </CardHeader>
+                <div className="flex flex-col gap-3">
+                  <CardContent
+                    onClick={() => handleAnswer(quiz.optionA, 0)}
+                    className={cn(
+                      "border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer",
+                      selected === 0
+                        ? "bg-blue-600 hover:bg-blue-600"
+                        : "bg-white"
+                    )}
+                  >
+                    {quiz.optionA}
+                  </CardContent>
+                  <CardContent
+                    onClick={() => handleAnswer(quiz.optionB, 1)}
+                    className={cn(
+                      "border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer",
+                      selected === 1
+                        ? "bg-blue-600 hover:bg-blue-600"
+                        : "bg-white"
+                    )}
+                  >
+                    {quiz.optionB}
+                  </CardContent>
+                  <CardContent
+                    onClick={() => handleAnswer(quiz.optionC, 2)}
+                    className={cn(
+                      "border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer",
+                      selected === 2
+                        ? "bg-blue-600 hover:bg-blue-600"
+                        : "bg-white"
+                    )}
+                  >
+                    {quiz.optionC}
+                  </CardContent>
+                  <CardContent
+                    onClick={() => handleAnswer(quiz.optionD, 3)}
+                    className={cn(
+                      "border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer",
+                      selected === 3
+                        ? "bg-blue-600 hover:bg-blue-600"
+                        : "bg-white"
+                    )}
+                  >
+                    {quiz.optionD}
+                  </CardContent>
+                </div>
+                <div className="w-full float-end">
+                  <Button
+                    className="w-full md:w-[100px] py-6 hover:bg-gradient-to-r hover:from-blue-600 hover:to-fuchsia-600"
+                    onClick={handleNext}
+                  >
+                    Next
+                  </Button>
+                </div>
+              </Card>
             </div>
           </div>
         ) : (
-          <div className="flex flex-col justify-center items-center  h-screen">
+          <div className="flex flex-col justify-center items-center h-screen">
             <div className="bg-gradient-to-r from-blue-600 to-fuchsia-600 p-[3px] rounded-md">
               <Card>
                 <CardHeader>
@@ -140,19 +117,18 @@ const QuizPage = () => {
                 </CardHeader>
                 <CardContent className="text-2xl font-bold">
                   <h1>
-                    Total Marks: {score} /{data.length}
+                    Total Marks: {score} / {data.length}
                   </h1>
                 </CardContent>
-                <CardContent className="text-2xl font-bold ">
-                  <h1>Percentage: {(score / 15) * 100}%</h1>
+                <CardContent className="text-2xl font-bold">
+                  <h1>Percentage: {(score / data.length) * 100}%</h1>
                 </CardContent>
                 <CardFooter>
                   <Button
-                    className={
-                      "hover:bg-gradient-to-r hover:from-blue-600 hover:to-fuchsia-600"
-                    }
+                    className="hover:bg-gradient-to-r hover:from-blue-600 hover:to-fuchsia-600"
                     onClick={() => {
                       setIndex(0);
+                      setScore(0);
                     }}
                   >
                     Restart Quiz
@@ -168,3 +144,174 @@ const QuizPage = () => {
 };
 
 export default QuizPage;
+
+// "use client";
+// import { Button } from "../../components/ui/button";
+// import { data } from "./questions";
+// import { useState } from "react";
+// import {
+//   Card,
+//   CardContent,
+//   CardFooter,
+//   CardHeader,
+// } from "@/components/ui/card";
+// import { cn } from "@/lib/utils";
+
+// const QuizPage = () => {
+//   const [index, setIndex] = useState(0);
+//   const [selected, setSelected] = useState();
+//   const [score, setScore] = useState(0);
+//   const quiz = data[index];
+//   const HandleIndex = () => {
+//     setSelected();
+//     setIndex(index + 1);
+//   };
+//   return (
+//     <div className="w-full h-screen bg-slate-300">
+//       <div className="">
+//         {index < 15 ? (
+//           <div>
+//             <div className="flex flex-col gap-2 justify-center items-center py-3">
+//               <h1 className="text-4xl font-bold font-serif text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-fuchsia-600">
+//                 Quiz Page
+//               </h1>
+//               <h3 className="text-2xl font-semibold ">
+//                 Questions{data.length}
+//               </h3>
+//             </div>
+
+//             <div className="w-full">
+//               <div>
+//                 <Card className={"w-full py-4 px-4 flex flex-col gap-7"}>
+//                   <div>
+//                     <CardHeader
+//                       className={
+//                         "text-lg sm:text-lg md:text-3xl font-bold px-1 h-20"
+//                       }
+//                     >
+//                       <h1>
+//                         {index + 1}.{quiz.question}
+//                       </h1>
+//                     </CardHeader>
+//                   </div>
+//                   <div className="flex flex-col gap-3">
+//                     <CardContent
+//                       onClick={() => {
+//                         quiz.optionA == quiz.answer
+//                           ? setScore(score + 1)
+//                           : null;
+//                         setSelected(0);
+//                       }}
+//                       className={cn(
+//                         `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer `,
+//                         selected == 0
+//                           ? "bg-blue-600 hover:bg-blue-600"
+//                           : "bg-white"
+//                       )}
+//                     >
+//                       {quiz.optionA}
+//                     </CardContent>
+//                     <CardContent
+//                       onClick={() => {
+//                         quiz.optionB == quiz.answer
+//                           ? setScore(score + 1)
+//                           : null;
+//                         setSelected(1);
+//                       }}
+//                       className={cn(
+//                         `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer `,
+//                         selected == 1
+//                           ? "bg-blue-600 hover:bg-blue-600"
+//                           : "bg-white"
+//                       )}
+//                     >
+//                       {quiz.optionB}
+//                     </CardContent>
+//                     <CardContent
+//                       onClick={() => {
+//                         quiz.optionC == quiz.answer
+//                           ? setScore(score + 1)
+//                           : null;
+//                         setSelected(2);
+//                       }}
+//                       className={cn(
+//                         `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer`,
+//                         selected == 2
+//                           ? "bg-blue-600 hover:bg-blue-600"
+//                           : "bg-white"
+//                       )}
+//                     >
+//                       {quiz.optionC}
+//                     </CardContent>
+//                     <CardContent
+//                       onClick={() => {
+//                         quiz.optionD == quiz.answer
+//                           ? setScore(score + 1)
+//                           : null;
+//                         setSelected(3);
+//                       }}
+//                       className={cn(
+//                         `border-2 border-slate-300 py-3 hover:bg-slate-300 cursor-pointer`,
+//                         selected == 3
+//                           ? "bg-blue-600 hover:bg-blue-600"
+//                           : "bg-white"
+//                       )}
+//                     >
+//                       {quiz.optionD}
+//                     </CardContent>
+//                   </div>
+//                   <div className=" w-full float-end">
+//                     <Button
+//                       className={
+//                         "w-full md:w-[100px] py-6 hover:bg-gradient-to-r hover:from-blue-600 hover:to-fuchsia-600"
+//                       }
+//                       onClick={() => {
+//                         HandleIndex();
+//                       }}
+//                     >
+//                       Next
+//                     </Button>
+//                   </div>
+//                 </Card>
+//               </div>
+//             </div>
+//           </div>
+//         ) : (
+//           <div className="flex flex-col justify-center items-center  h-screen">
+//             <div className="bg-gradient-to-r from-blue-600 to-fuchsia-600 p-[3px] rounded-md">
+//               <Card>
+//                 <CardHeader>
+//                   <h1 className="text-4xl font-bold font-serif text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-fuchsia-600">
+//                     Results
+//                   </h1>
+//                 </CardHeader>
+//                 <CardContent className="text-2xl font-bold">
+//                   <h1>
+//                     Total Marks: {score} /{data.length}
+//                   </h1>
+//                 </CardContent>
+//                 <CardContent className="text-2xl font-bold ">
+//                   <h1>Percentage: {(score / 15) * 100}%</h1>
+//                 </CardContent>
+//                 <CardFooter>
+//                   <Button
+//                     className={
+//                       "hover:bg-gradient-to-r hover:from-blue-600 hover:to-fuchsia-600"
+//                     }
+//                     onClick={() => {
+//                       setIndex(0);
+//                     }}
+//                   >
+//                     Restart Quiz
+//                   </Button>
+//                 </CardFooter>
+//               </Card>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default QuizPage;
